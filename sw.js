6af7d6c167380d56391dec3b09559218e188b969
storeapp <latynx@gmail.com>
2026-07-22 12:59:33 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,11 @@
-const CACHE_NAME = 'gasapp-v1';
-const urlsToCache = [
-    '/',
-    'index.html',
-    'add-fuel.html',
-    'add-service.html',
-    'edit-service.html',
-    'history.html',
-    'css/style.css',
-    'js/db.js',
-    'js/dashboard.js',
-    'js/add-fuel.js',
-    'js/add-service.js',
-    'js/edit-service.js',
-    'js/history.js',
-    'js/utils.js',
-    'manifest.json'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+self.addEventListener('activate', function(e) {
+  return self.clients.claim();
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request));
 });
